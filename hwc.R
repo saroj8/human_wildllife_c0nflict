af22acdef66642b0dc497b127c99fbe12872f9d4
Saroj <saroj8>
2022-10-27 21:25:14 +0545

    trend line added

--- hwc.R
+++ hwc.R
@@ -1,8 +1,10 @@
 ## Human Wildlife conflict in Chitwan National Park----
 ### Saroj Kandel, 10-23-2022, sarojkndl@gmail.com----
+###libraries----
 library(dplyr)
 library(readxl)
 library(tidyverse)
+library(ggplot2)
 data1<-read_xlsx("hwc_data.xlsx" )
 data1
 #making data frame----
@@ -59,8 +61,7 @@ ctable
  btable
  
  
- ggplot(onlyrhino,aes(x=Year))+
-   geom_bar()
+
  
  
  
@@ -68,9 +69,9 @@ ctable
  subset(data3, Species=="Rhino")
 # we get error that " chi-squared approximation may be incorrect"
 # so we are using Fisher's exact test----
-test_f<- fisher.test(ctable, workspace = 2e9) # error shown that work space is less so we increase work space o 2e9
-test_f
-test_f$p.value
+##test_f<- fisher.test(ctable, workspace = 2e9) # error shown that work space is less so we increase work space o 2e9
+#test_f
+#test_f$p.value
 
 # temporal pattern of human injuries and fatalities----
 
@@ -120,18 +121,61 @@ facet
 
 facet+facet_wrap(~ Species, ncol=2)
 
+# adding a season column----
 
+data4<-data3 %>% 
+  mutate(
+    Season= case_when(
+      Month %in% 9:11 ~ "Autumn",
+      Month %in% c(12,1,2)~ "Winter",
+      Month %in% 3:5 ~ "Spring",
+      TRUE~ "Summer"
+    )
+  )
+ head(data4)
+#plot the data of season into graph
+ ggplot(data4, aes(x=Season))+
+   geom_bar()
+ 
+ # see the wildlife events species wise-----
+ 
+species_table<- table(data4$Species, data4$Year)
+species_table 
+ 
+ rhino_table<- table(data4$Year, data4$Species=="Rhino")   
+rhino_table
 
+#making data frame for data4------
+data5<- data.frame(data4)
+data5
+head(data5)
+# asking for help with dput-----
 
+my_data_compressed<- data5[1:6,]
 
+# trying tibble for data wrangling----
+tdata<-tibble(data5)
+tdata
 
+#count the data for factors----
+species_freq<- count(tdata$Species)
+species_freq 
+# changing year that was in numeric to factor and showing in graph
+tdata$Year<- as.factor(tdata$Year)
+tdata
+plot(tdata$Year)
 
+a<-count(tdata$Year)
+a
 
+# lets remove 2021 from the data as we dont have complete data for the year.
+ b<- data.frame(Year=factor(c(2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020)),
+                Attacks=c(11,38,32,38,36,31,27,55,24,94,63,56))
+b
 
-
-
-
-
+ggplot(b, aes(x=Year, y=Attacks, group=1)) +
+  geom_path()+
+  geom_point()
 
 
 
